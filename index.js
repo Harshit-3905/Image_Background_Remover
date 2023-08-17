ac4ac1c1d82b032f7cda4c8f65272ceb8d986cfb
Harshit-3905 <harshitjoshileet@gmail.com>
2023-08-17 12:35:51 +0530

    Updated JS

---
+++ index.js
@@ -0,0 +1,39 @@
+const image = document.querySelector("#uploadimg");
+const input = document.querySelector("input");
+input.addEventListener("change",() => {
+    image.src = URL.createObjectURL(input.files[0]);
+});
+let downloadURL;
+const downloadimage = document.querySelector("#downloadimg");
+const convert = document.querySelector("#convertbutton");
+convert.addEventListener("click",()=> {
+     const formData = new FormData();
+     formData.append("image_file" , input.files[0]);
+     const apikey = "7385573d880cecda3f397dfaaa9e7a699ae140c1"; 
+     fetch("https://sdk.photoroom.com/v1/segment",{
+        method : "POST",
+        headers:{
+            "X-Api-Key": apikey,
+        },
+        body :formData
+     })
+     .then(function(response){
+        return response.blob();
+     })
+     .then(function(blob){
+        const url = URL.createObjectURL(blob);
+        downloadURL = url;
+        downloadimage.src = url;
+     })
+     .catch();
+});
+
+const download = document.querySelector("#download");
+download.addEventListener("click",()=>{
+      var anchorElement = document.createElement('a'); 
+      anchorElement.href = downloadURL;
+      anchorElement.download = 'no-bg.png';
+      document.body.appendChild(anchorElement);
+      anchorElement.click();
+      document.body.removeChild(anchorElement);
+})
